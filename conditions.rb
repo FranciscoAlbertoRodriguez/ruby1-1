cb82d6f4d650140dcac944818254a2f68aebbfb4
Cindy Howard <howardcy@lewisu.edu>
2017-02-02 18:04:30 +0000

    thursday code

--- conditions.rb
+++ conditions.rb
@@ -7,12 +7,20 @@ elsif score >= 80
     grade = "B"
 elsif score >= 70
     grade = "C"
-elsif grade >= 60
+elsif score >= 60
     grade = "D"
 else
     grade = "F"
 end
 puts "You earned a #{grade}"
+ 
+ hungry = false
+ unless hungry
+    puts "write code"
+else
+    puts "eat something"
+end
     
-    
+puts "Winter" if hungry
+puts "Write code" unless hungry
     
